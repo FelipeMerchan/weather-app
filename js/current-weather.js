370d9172e0f900ffc63bbf792b6376eecf6ec3d1
Felipe Merchan <felipe-sc@hotmail.es>
2021-10-02 14:09:16 -0500

    feat: render city of origin

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,10 +1,16 @@
 import weather from "../data/current-weather.js"
 
+function setCurrentCity($el, city) {
+  $el.textContent = city
+}
+
 function configCurrentWeather(weather) {
-  
+  //city
+  const $currentWeatherCity = document.querySelector("#current-weather-city")
+  const city = weather.name
+  setCurrentCity($currentWeatherCity, city)
 }
 
 export function currentWeather() {
   configCurrentWeather(weather)
-  console.log(weather)
 }
