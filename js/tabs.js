0eab3407c00ec4ecc9ee21d898842409fc612649
Felipe Merchan <felipe-sc@hotmail.es>
2021-10-02 20:02:57 -0500

    feat: render weekdays

---
+++ js/tabs.js
@@ -0,0 +1,32 @@
+const $tabContainer = document.querySelector("#tabs")
+const $tabListAsArray = [...$tabContainer.querySelectorAll(".tab")]
+
+const today = new Date()
+let weekDay = today.getDay()
+
+const week = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miércoles",
+  "Jueves",
+  "Viernes",
+  "Sábado",
+]
+
+function nextDay(day) {
+  if (day === 6) {
+    return 0
+  }
+  return ++day
+}
+
+$tabListAsArray.forEach(($tab, index) => {
+  if (index === 0) {
+    $tab.textContent = "Hoy"
+    weekDay = nextDay(weekDay)
+    return false
+  }
+  $tab.textContent = week[weekDay]
+  weekDay = nextDay(weekDay)
+})
